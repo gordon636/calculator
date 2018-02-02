c9c3ce0ea2bbd4ecb85131bf220094e503743c71
gordon636 <gordonwhite636@gmail.com>
2018-02-02 13:51:20 -0500

    More progress...

--- manual.js
+++ manual.js
@@ -3,3 +3,14 @@ const Manual = function() {
   this.points = 27;
   this.base = 8;
 };
+
+// Try to increment request.stat and charge request.points
+Manual.prototype.increment = function(request){
+  request.stat++;
+  request.points--;
+};
+
+Manual.prototype.decrement = function(request){
+  request.stat--;
+  request.points++;
+};
